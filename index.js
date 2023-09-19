343a48b941a3af612255b872e2ff46ea1ff60835
Ankit Dahiya <71880797+ankitd2002@users.noreply.github.com>
2023-09-18 23:57:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -25,18 +25,18 @@ console.log('Namaste Dunia version4');
 if(marks >= 90) {
      console.log('A');
  }
-// else if(marks >= 80) {
-//     console.log('B');
-// }
-// else if(marks >=70) {
-//     console.log('C');
-// }
-// else if(marks >= 60) {
-//     console.log('D');
-// }
-// else {
-//     console.log('E');
-//}
+ else if(marks >= 80) {
+     console.log('B');
+}
+ else if(marks >=70) {
+    console.log('C');
+ }
+ else if(marks >= 60) {
+    console.log('D');
+ }
+ else {
+    console.log('E');
+}
 
  let num = 1;
 
