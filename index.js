d1a664ed0de8e3b01bac2b039d883ac0b71561ba
Ankit Dahiya <71880797+ankitd2002@users.noreply.github.com>
2023-09-18 23:56:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // line below is used to print on the console window
 console.log('Namaste Dunia version4');
 
-// let a = true;
-// console.log(a);
+ let a = true;
+ console.log(a);
 
-// const num = 12;
-// //num = 13;
+ const num = 12;
+ num = 13;
 
-// let lastName = 3;
-// console.log(lastName);
+ let lastName = 3;
+ console.log(lastName);
 
-// lastName = true;
-// console.log(lastName);
+ lastName = true;
+ console.log(lastName);
 
-// console.log(5 !== 3);
+ console.log(5 !== 3);
 
-// let age = 27;
-// let status = (age >= 18) ? 'Yes Vote' : 'No Vote';
+ let age = 27;
+ let status = (age >= 18) ? 'Yes Vote' : 'No Vote';
 
-// console.log(status);
+ console.log(status);
 
-// let marks = 12;
+ let marks = 12;
 
-// if(marks >= 90) {
-//     console.log('A');
-// }
+if(marks >= 90) {
+     console.log('A');
+ }
 // else if(marks >= 80) {
 //     console.log('B');
 // }
@@ -38,29 +38,29 @@ console.log('Namaste Dunia version4');
 //     console.log('E');
 //}
 
-// let num = 1;
+ let num = 1;
 
-// switch(num) {
-//     case 1: console.log('A');
-//     break;
-//     case 2: console.log('B');
-//     break;
-//     case 3: console.log('C');
-//     break;
-//     default: console.log('D');
-// }
+ switch(num) {
+     case 1: console.log('A');
+     break;
+     case 2: console.log('B');
+     break;
+     case 3: console.log('C');
+    break;
+     default: console.log('D');
+ }
 
 
-// for(let i = 1; i<=5; i=i+1) {
-//     console.log("Babbar" + i);
-// }
+ for(let i = 1; i<=5; i=i+1) {
+    console.log("Babbar" + i);
+ }
 
-// let x = 1;
+ let x = 1;
 
-// while(x <= 5) {
-//     console.log(x);
-//     x++;
-// }
+ while(x <= 5) {
+     console.log(x);
+     x++;
+ }
 
 let y = 1;
 
